db0ada77c864887d7ca3fe35113dc67976631f99
pdelepine <delepine.py@gmail.com>
2019-11-30 13:48:58 +0100

    ajout sources citybuilder

---
+++ city/parser/main.cc
@@ -0,0 +1,23 @@
+#include "parser.hpp"
+#include "scanner.hh"
+#include "driver.hh"
+
+#include <iostream>
+#include <fstream>
+
+#include <cstring>
+
+int main( int  argc, char* argv[]) {
+    Driver driver;
+    std::ifstream fichier("01.city");
+    std::string ligne;
+
+    Scanner scanner(fichier, std::cout);
+    yy::Parser parser(scanner, driver);
+
+    parser.parse();
+    while(getline(fichier,ligne)){
+        std::cout<<ligne<<std::endl;
+    }
+    return 0;
+}
